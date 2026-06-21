416b3a68c89f1547202648931f18020e6ecd40fb
Maria Eduarda <157310243+MariaEduardatvs@users.noreply.github.com>
2026-06-21 16:52:58 +0100

    Add files via upload
    first commit

---
+++ KPM/Database/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE kmp_task_management;
+
+USE kmp_task_management;
+
+-- USERS
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
