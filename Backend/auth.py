60952d3ab98f300bc03bb506205867f4c7589c0f
Maria Eduarda <maria.eduardatavares.ie@gmail.com>
2026-07-12 18:00:58 +0100

    User Registration completed

--- Backend/auth.py
+++ Backend/auth.py
@@ -1,3 +1,8 @@
+#REGISTRAR USUÁRIO: 
+# Receive username and password
+# Checks if the user already exists
+#If it exists, it returns error 400
+
 # blueprint for organize the routes in different files 
 #request to get the data from the user
 #jsonify to convert python to json 
@@ -24,3 +29,34 @@ def register():
 
 cursor = conn.cursor(dictionary=True) #cursor to execute sql and  dictionary=True to change from tupla to dictionary 
 
+#check if the user name already exists in the database
+cursor.execute(
+        "SELECT * FROM users WHERE username=%s",
+        (username,)
+    )
+
+#get the first name
+user = cursor.fetchone()
+
+#return an error if the user already exists
+if user:
+        return jsonify({
+            "message": "Username already exists."
+        }), 400
+
+#if dont exists it will insert the new user name
+cursor.execute(
+        """
+        INSERT INTO users(username,password)
+        VALUES(%s,%s)
+        """,
+        (username, password)
+    )
+
+#save it in the database
+conn.commit()
+
+#success message
+return jsonify({
+        "message": "User registered successfully!"
+    })
